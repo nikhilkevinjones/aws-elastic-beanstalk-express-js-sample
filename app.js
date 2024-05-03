b487dd40881a5155cc898299f78717342394a99c
Nikhil Jones <nikhil.jones@fau.de>
2024-05-03 23:09:50 +0200

    full test of pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello nkj welcome to AWS!'));
+app.get('/', (req, res) => res.send('Hello nkj welcome to AWS, now i am testing the full change!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
